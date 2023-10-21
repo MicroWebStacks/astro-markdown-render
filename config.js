fb866ce0ddd0e22b2d5e600c4f994aa28f5c0912
wass <wassim.filali@gmail.com>
2023-10-21 14:52:41 +0200

    fix root dir in prod

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 import { fileURLToPath } from 'url';
-import {dirname} from 'path'
+import {dirname, join} from 'path'
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+let __dirname = dirname(__filename);
+
+if(import.meta.env.MODE == "production"){
+	__dirname = join(__dirname,'../../..')
+}
 
 const config = {
     rootdir: __dirname,
@@ -11,6 +15,8 @@ const config = {
     plantuml_server: "https://www.plantuml.com/plantuml/svg",
     kroki_server: "https://kroki.io",
 }
+
+console.log(config)
 
 export {
     config
