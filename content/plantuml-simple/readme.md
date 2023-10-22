a2bd251554abe35ac748a6be03b2e43264e881ee
wass <wassim.filali@gmail.com>
2023-10-22 14:18:50 +0200

    working on copy code from highlighter

---
+++ content/plantuml-simple/readme.md
@@ -0,0 +1,12 @@
+---
+title: Plantuml Simple
+---
+```plantuml long-sequence
+@startuml
+Michael -> Maria : Message(Hello)
+Maria -> Marco : Message(Hi)
+Marco -> Miranda : Message(Hi there)
+Miranda -> Mario : Message(Hallo)
+Mario -> Mathilda: Message(Hallo)
+@enduml
+```
--- src/components/nodes/code/diagram.js
+++ src/components/nodes/code/diagram.js
@@ -9,14 +9,15 @@ async function diagram_cache(code,generator){
     const file_exists = await exists(file_path)
     if(file_exists){
         console.log(`* returning diagram from cache '${file_path}'`)
-        return await fs.readFile(file_path,'utf-8')
+        const svg_text = await fs.readFile(file_path,'utf-8')
+        return {svg_text, hash}
     }else{
         console.log(`* generating diagram as not in cache`)
         const svg_text = await generator(code)
         await save_file(file_path,svg_text)
         const code_path = join(config.rootdir, `gen/diagrams/${hash}/code.puml`)
         await save_file(code_path,svg_text)
-        return svg_text
+        return {svg_text, hash}
     }
 }
 
